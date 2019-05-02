e6016e2f6bec14cb97f5081a333db4455640109a
Shreya Donepudi <shreya@donepudis.com>
2019-05-02 12:48:56 -0500

    Fix SendGrid (again)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,11 +66,11 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://pacific-scrubland-16239.herokuapp.com'
+  host = 'pacific-scrubland-16239.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port => '2525',
+    :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGIRD_USERNAME'],
     :password => ENV['SENDGIRD_PASSWORD'],
