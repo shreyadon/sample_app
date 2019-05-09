fdca11008a2520da7b0f747d0749d179f521c181
Shreya Donepudi <shreya@donepudis.com>
2019-05-09 12:34:38 -0500

    Add figaro gem

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'coffee-rails', '4.2.2'
 gem 'jquery-rails', '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
+gem 'figaro'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
@@ -38,7 +39,7 @@ end
 
 group :production do
   gem 'pg', '0.20.0'
-  gem 'fog', '1.42'
+  gem 'fog-aws'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
@@ -8,6 +9,5 @@ if Rails.env.production?
       :region => ENV['S3_REGION']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
-    config.fog_provider = 'fog/aws'
   end
 end
